7c05c5abb8022131936071da4c6e03ad57f31dd1
UltraJohn <johnh.dk93@gmail.com>
2018-01-07 13:07:43 +0100

    Add project files.

---
+++ SocketConsoleClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			TcpClient client;
+			NetworkStream stream;
+			StreamReader sr;
+			StreamWriter sw;
+
+			Console.WriteLine("Press Any Key to connect");
+			Console.ReadKey();
+			client = new TcpClient("127.0.0.1", 12345);
+			stream = client.GetStream();
+			sr = new StreamReader(stream);
+			sw = new StreamWriter(stream);
+			Console.WriteLine("Connected.");
+			Console.WriteLine("Usage:    dir [path]     subdir [path]");
+			while (true)
+			{
+				Console.WriteLine("Enter your query:");
+				string line = Console.ReadLine();
+				sw.WriteLine(line);
+				sw.Flush();
+				Console.WriteLine(sr.ReadLine());
+			}
+		}
+    }
+}
---
+++ SocketConsoleServer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Waiting for connection...");
+			TcpListener listener = new TcpListener(IPAddress.Any, 12345);
+			listener.Start();
+			Socket client = listener.AcceptSocket();
+			Console.WriteLine("Client connected");
+			NetworkStream stream = new NetworkStream(client);
+			StreamReader sr = new StreamReader(stream);
+			StreamWriter sw = new StreamWriter(stream);
+			while (true)
+			{
+				string request = sr.ReadLine();
+				Console.WriteLine("got request: "+request);
+				string[] split = request.Split(' ');
+				DirectoryInfo info;
+				switch (split[0])
+				{
+					case "dir":
+						info = new DirectoryInfo(split[1]);
+						if (info.Exists)
+						{
+							sw.WriteLine(split[1] + " exists, and was created on " + info.CreationTime);
+						}
+						else
+						{
+							sw.WriteLine(split[1]+" was not found. Please try again.");
+						}
+						sw.Flush();
+						break;
+					case "subdir":
+						info = new DirectoryInfo(split[1]);
+						if (info.Exists)
+						{
+							string response = split[1] + " exists, and was created on " + info.CreationTime;
+							DirectoryInfo[] dirs = info.GetDirectories();
+							if(dirs.Length > 0)
+							{
+								response += " The subdirectories are: ";
+								foreach (var dir in dirs)
+								{
+									response += dir.Name+", ";
+								}
+								response = response.Trim(' ').Trim(',');
+							}
+							else
+							{
+								response += " There are no subdirectories";
+							}
+							sw.WriteLine(response);
+						}
+						else
+						{
+							sw.WriteLine(split[1] + " was not found. Please try again.");
+						}
+						sw.Flush();
+						break;
+					default:
+						break;
+				}
+			}
+        }
+    }
+}
